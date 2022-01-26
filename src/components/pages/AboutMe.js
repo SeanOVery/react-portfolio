f96b3574b8d274e14bb8e69e1ebc16f4f7651f82
SeanOVeryMac <seanovery1@gmail.com>
2022-01-26 17:19:28 -0500

    some more details and styling added

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -4,14 +4,14 @@ import '../../styles/AboutMe.css'
 export default function AboutMe() {
   return (
     <div className='aboutMeContainer'>
-      <h1>About Me</h1>
+      <h2>About me</h2>
+      <p>I am an aspiring web developer currently living in Kennesaw, Georgia. I started out working in IT 10 years ago focusing mostly on computer hardware. Computer hardware is still where most of my experience lies but I've had a side interest in software the whole time. I'm now developing those skills and looking to potentially build a career in software.
+      </p>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      Beyond technology I've worked in a variety of fields. Some tech adjacent for example running a project to digitize a companies old files. Some not, like working in winemaking for a few wineries.
+      </p>
+      <p>
+      Outside of work I'm an avid sports fan. Mostly a soccer fan but I grew up in Boise, Idaho and am a big Boise State football fan. I follow esports as well with my favorite game being Rocket League.
       </p>
     </div>
   );
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -5,17 +5,23 @@ export default function Resume() {
   return (
     <div className='resumeContainer'>
       <h1>Resume</h1>
+      <h2>Technologies</h2>
+      <ul>
+        <li>Javascript</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>MongoDB</li>
+        <li>SQL</li>
+        <li>Express</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+      <h2>Experience</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque
-        velit, lobortis ut magna varius, blandit rhoncus sem. Morbi lacinia nisi
-        ac dui fermentum, sed luctus urna tincidunt. Etiam ut feugiat ex. Cras
-        non risus mi. Curabitur mattis rutrum ipsum, ut aliquet urna imperdiet
-        ac. Sed nec nulla aliquam, bibendum odio eget, vestibulum tortor. Cras
-        rutrum ligula in tincidunt commodo. Morbi sit amet mollis orci, in
-        tristique ex. Donec nec ornare elit. Donec blandit est sed risus feugiat
-        porttitor. Vestibulum molestie hendrerit massa non consequat. Vestibulum
-        vitae lorem tortor. In elementum ultricies tempus. Interdum et malesuada
-        fames ac ante ipsum primis in faucibus.
+      </p>
+      <h2>Link to Resume</h2>
+      <p>
+        todo: put resume link here
       </p>
     </div>
   );
